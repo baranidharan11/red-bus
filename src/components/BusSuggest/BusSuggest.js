7ffbf5a19998bf1bb956354ef9aed5ab0d05fef5
baranidharan11 <baraniskt11@gmail.com>
2024-05-24 22:51:42 +0530

    input box

--- src/components/BusSuggest/BusSuggest.js
+++ src/components/BusSuggest/BusSuggest.js
@@ -4,13 +4,24 @@ export default function BusSuggest() {
   return (
     <div className='bus-suggest'>
       <div className='body-header'>India's No. 1 Online Bus Ticket Booking Site</div>
-<div className='display-flex'>
+<div className='display-flex margin-150px'>
     <div className='search-from'>
         <div>From</div>
+        <input className='search-input' placeholder='Enter departure'></input>
     </div>
-    <div className='search-to'></div>
-    <div className='date'></div>
-    <div className='search-button'></div>
+    <div className='search-to'>
+      <div>To</div>
+      <input className='search-input' placeholder='Enter destination'></input>
+    </div>
+    <div className='date'>
+      <div>
+        Select Date
+      </div>
+      <input type="date" className='search-input'></input>
+    </div>
+    <div className='search-button'>
+      <div>Search</div>
+    </div>
     </div>
    </div>
   )
